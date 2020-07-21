cef2fcd27ad963424489afa1a0cad8a5da56318d
abdulazeez-dolapo <abdulrafiua10@gmail.com>
2020-07-22 00:39:20 +0100

    add login page

--- pages/login.vue
+++ pages/login.vue
@@ -3,9 +3,9 @@
     <v-col class="pa-0" cols="12" sm="9" md="6">
       <v-card class="pa-3">
         <p class="ma-0 text-h4 pb-5 black--text text-center">
-          Create Account
+          Login
         </p>
-        <v-form ref="registerForm">
+        <v-form ref="loginForm">
           <v-text-field
             dense
             type="text"
@@ -34,19 +34,19 @@
               depressed
               color="primary"
               block
-              @click.prevent="register"
-              >Sign Up</v-btn
+              @click.prevent="login"
+              >Login</v-btn
             >
           </v-row>
         </v-form>
 
         <v-row class="ma-0 justify-center align-center">
           <span class="ma-0">
-            Already have an account?
+            Don't have an account?
           </span>
           <v-spacer></v-spacer>
-          <v-btn color="green" class="white--text" to="/login" depressed
-            >Login</v-btn
+          <v-btn color="green" class="white--text" to="/register" depressed
+            >Sign Up</v-btn
           >
         </v-row>
       </v-card>
@@ -65,7 +65,6 @@
 import notification from "~/components/Notification";
 import loading from "~/components/Loading";
 import { handler } from "~/mixins/handler";
-
 export default {
   layout: "authentication",
   components: {
@@ -81,43 +80,35 @@ export default {
       timer: "",
       username: "",
       password: "",
-      usernameRules: [
-        v => !!v || "Username is required",
-        v => v.length <= 8 || "Username must be 8 characters or less"
-      ],
       passwordRules: [
         v => !!v || "Password is required",
-        v => v.length >= 8 || "Password must be at least 8 characters",
         v => v.length <= 16 || "Password must be not be more than 16 characters"
+      ],
+      usernameRules: [
+        v => !!v || "Username is required",
+        v => v.length <= 16 || "Username must be not be more than 16 characters"
       ]
     };
   },
   methods: {
-    async register() {
+    async login() {
       try {
         // Validate form inputs
-        if (!this.$refs.registerForm.validate()) return;
-        this.loadingText = "Creating account";
+        if (!this.$refs.loginForm.validate()) return;
+        this.loadingText = "Logging in";
         this.loadingDialog = true;
         this.notificationDialog = false;
         this.notificationText = "";
 
-        const payload = {
-          username: this.username,
-          password: this.password
-        };
+        const res = await this.$auth.loginWith("local", {
+          data: {
+            username: this.username,
+            password: this.password
+          }
+        });
 
-        let res = await this.$axios.$post("/api/auth/register", payload);
-
-        // If succesfully signed up, login user
-        if (res.success) {
-          const res = await this.$auth.loginWith("local", {
-            data: payload
-          });
-
-          this.loadingDialog = false;
-          this.$router.push("/");
-        }
+        this.loadingDialog = false;
+        this.$router.push("/");
       } catch (error) {
         this.handleError(error);
       }
