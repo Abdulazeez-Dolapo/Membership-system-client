bd719764686fed83c57fca7304a85534bb01ca71
abdulazeez-dolapo <abdulrafiua10@gmail.com>
2020-07-22 07:47:35 +0100

    add a landing page to the app

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,17 @@
 <template>
-  <div>
-    hello
+  <div class="home">
+    <v-row class="ma-0 pl-1 justify-center justify-md-start">
+      <p class="ma-0 text-h3 text-center pb-2 font-weight-bold">
+        This is the landing page of the App
+      </p>
+    </v-row>
+
+    <v-row class="ma-0 pl-1 justify-center justify-md-start">
+      <p class="ma-0 text-h5 text-center pb-2 font-weight-bold">
+        You can click on your username on the navbar or the sidebar to be able
+        to go to the profile page
+      </p>
+    </v-row>
   </div>
 </template>
 
--- pages/profile.vue
+++ pages/profile.vue
@@ -1,17 +1,29 @@
 <template>
-  <div>
+  <v-container fluid>
     <v-row class="ma-0 pl-1 justify-center justify-md-start">
-      <p class="ma-0 text-h3 pb-2 font-weight-bold">
+      <p class="ma-0 text-h3 text-center pb-2 font-weight-bold">
+        Hi {{ user.username }} !!
+      </p>
+    </v-row>
+
+    <v-row class="ma-0 pl-1 justify-center justify-md-start">
+      <p class="ma-0 text-h3 text-center pb-2 font-weight-bold">
         Welcome to The Members App {{ user.username }}
       </p>
     </v-row>
 
     <v-row class="ma-0 pl-1 justify-center justify-md-start">
-      <p class="ma-0 text-h5 pb-2 font-weight-bold">
+      <p class="ma-0 text-h5 text-center pb-2 font-weight-bold">
+        Here is your profile page
+      </p>
+    </v-row>
+
+    <v-row class="ma-0 pl-1 justify-center justify-md-start">
+      <p class="ma-0 text-h5 text-center pb-2 font-weight-bold">
         We hope you enjoy your stay here
       </p>
     </v-row>
-  </div>
+  </v-container>
 </template>
 
 <script>
